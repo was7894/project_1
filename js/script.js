43e352f38baa98509fadd00d118f4c539fa90578
was7894 <was7894@nate.com>
2022-12-26 02:16:28 +0900

    update : header__bottom 부분 depth1 hover; background 색변화

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+//팝업
 document.querySelector("#layer button").addEventListener("click", function () {
   document.querySelector("#layer").style.display = "none";
+});
+
+//header__bottom 색변화
+$(".dep1 li").mouseover(function () {
+  // $(this).closest(".header__bottom").stop().animate({ backgroundColor: "green" }, 1500, "linear");
+
+  $(this).closest(".header__bottom").css("backgroundColor", "#2e363d");
+});
+$(".dep1 li").mouseout(function () {
+  $(this).closest(".header__bottom").css("backgroundColor", "rgb(46, 54, 61, 0.5)");
 });
