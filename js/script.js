a4e1312076a0eba345a63e2aad21e70c9f3df9db
was7894 <was7894@nate.com>
2022-12-26 21:33:00 +0900

    update : dep2부분 제이쿼리로 구현 완성

--- js/script.js
+++ js/script.js
@@ -4,11 +4,17 @@ document.querySelector("#layer button").addEventListener("click", function () {
 });
 
 //header__bottom 색변화
-$(".dep1 li").mouseover(function () {
-  // $(this).closest(".header__bottom").stop().animate({ backgroundColor: "green" }, 1500, "linear");
+$(".dep1 li")
+  .mouseover(function () {
+    // $(this).closest(".header__bottom").stop().animate({ backgroundColor: "green" }, 1500, "linear");
+    // $(this).closest(".header__bottom").css("backgroundColor", "#2e363d");
+    $(".header__bottom").css("backgroundColor", "#2e363d");
 
-  $(this).closest(".header__bottom").css("backgroundColor", "#2e363d");
-});
-$(".dep1 li").mouseout(function () {
-  $(this).closest(".header__bottom").css("backgroundColor", "rgb(46, 54, 61, 0.5)");
-});
+    $(this).find(".all_dep2").stop().slideDown(400);
+  })
+  .mouseout(function () {
+    // $(this).closest(".header__bottom").css("backgroundColor", "rgb(46, 54, 61, 0.5)");
+    $(".header__bottom").css("backgroundColor", "rgb(46, 54, 61, 0.5)");
+
+    $(this).find(".all_dep2").stop().slideUp(400);
+  });
