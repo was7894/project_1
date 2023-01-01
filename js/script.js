9f4f33d57cfe86d428e9deac6ae0bf9d0e487617
was7894 <was7894@nate.com>
2023-01-02 02:26:43 +0900

    update : section2 재생정지버튼및 버튼슬라이드 자동재생 일부 구현 / 클릭시 넘어가는부분 미구현

--- js/script.js
+++ js/script.js
@@ -19,21 +19,61 @@ $(".dep1 li")
   });
 
 /* section2 banner slides*/
-let sec2_slides = $(".popup_banner a img"); //6개 인덱스 저장
-// console.log(sec2_slides);
-let count = sec2_slides.length,
-  currentIndex = 0;
+let sec2_slides = $(".popup_banner .sec_visual2_list a img");
 
-sec2_slides.eq(currentIndex).fadeIn(); //첫 배너 fadein.
+let count = sec2_slides.length;
+let currentIndex = 0;
+let ctrl = $(".popup_banner .ctrl a img");
 
-setInterval(slidesMove, 4000);
+let button = $(".popup_banner .button_list li");
+
+sec2_slides.eq(currentIndex).fadeIn();
 
 function slidesMove() {
-  let nextIndex = (currentIndex + 1) % 6;
+  // console.log(btnIndex);
+  button.eq(currentIndex).find(".btn").removeClass("on");
+  let nextIndex = (currentIndex + 1) % count;
   sec2_slides.eq(currentIndex).fadeOut();
   sec2_slides.eq(nextIndex).fadeIn();
   currentIndex = nextIndex;
+  button.eq(currentIndex).find(".btn").addClass("on");
 }
 
+//timer 초기화
+let timer;
+let btnPlay = $(".btn_play");
+let btnStop = $(".btn_stop");
 
+// slide play button
+function slidePlay() {
+  timer = setInterval(slidesMove, 3000);
+  $(".btn_stop").addClass("on");
+  $(".btn_play").removeClass("on");
+}
+// slide stop button
+function slideStop() {
+  clearInterval(timer);
+  $(".btn_play").addClass("on");
+  $(".btn_stop").removeClass("on");
+}
 
+slidePlay();
+
+btnStop.click(function (e) {
+  e.preventDefault();
+  slideStop();
+});
+
+btnPlay.click(function (e) {
+  e.preventDefault();
+  slidePlay();
+});
+
+//control index btn
+button.click(function () {
+  btnIndex = $(this).index();
+  console.log(btnIndex);
+  button.removeClass("on");
+  $(this).eq(btnIndex).find(".btn").addClass("on");
+  slidesMove();
+});
