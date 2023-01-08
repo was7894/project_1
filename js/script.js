98d3691dd481527c25c08fe443d1e9ba8807323c
was7894 <was7894@nate.com>
2023-01-08 23:18:43 +0900

    update: section1~section4 720p 미디어쿼리부분 완료 (sns섹션 아쉬운부분 자의로 판단하여 수정)              section3 : 버튼슬라이드부분 구현              footer - quick버튼 동작부분완성

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.querySelector("#layer button").addEventListener("click", function () {
   document.querySelector("#layer").style.display = "none";
 });
 
-/*section1-------------------------------------------------------------------------- */
+/*section1--------------------------------------------------------------------------*/
 $(".dep1 li")
   .mouseover(function () {
     //header__bottom 색변화
@@ -19,15 +19,10 @@ $(".dep1 li")
   });
 // -----------------------------------------------------------------------------------
 /* section2 banner slides*/
+
+// let ctrl = $(".popup_banner .ctrl a img");
 let sec2_slides = $(".popup_banner .sec_visual2_list a img");
-let count = sec2_slides.length;
-
-let ctrl = $(".popup_banner .ctrl a img");
-
-let button = $(".popup_banner .button_list li");
 let currentIndex = 0;
-let btnIndex = 0;
-
 sec2_slides.eq(currentIndex).fadeIn();
 
 //timer 초기화
@@ -63,6 +58,9 @@ function slideStop() {
 slidePlay();
 
 //section2 버튼클릭슬라이드전환
+let button = $(".popup_banner .button_list li");
+let btnIndex = 0;
+
 button.click(function (e) {
   e.preventDefault();
   btnIndex = $(this).index();
@@ -82,11 +80,10 @@ button.click(function (e) {
 });
 
 // section2 : timer slide move
-function slidesMove() {
-  // console.log(`move${btnIndex}`);//btnIndex값 가져와서 거기서부터 실행하도록 만들어야됨
+let count = sec2_slides.length;
 
+function slidesMove() {
   button.find(".btn").removeClass("on"); //실행시 버튼 on효과 다 제거
-  // button.eq(btnIndex).find("btn").addClass("on"); //3
   let nextIndex = (currentIndex + 1) % count;
   sec2_slides.eq(currentIndex).fadeOut();
   sec2_slides.eq(nextIndex).fadeIn();
@@ -99,7 +96,7 @@ let tab_btn = $(".teb_menu>li>a");
 let tab_cont = $(".teb_menu>li>div");
 
 // 첫화면 active
-tab_btn.eq(0).addClass("active");
+// tab_btn.eq(0).addClass("active");
 tab_btn.eq(0).addClass("on");
 tab_cont.eq(0).show();
 
@@ -117,4 +114,101 @@ tab_btn.click(function (e) {
   // 밑줄
   tab_btn.removeClass("on");
   $this.addClass("on");
+});
+
+//section3 interview slides----------------------------------------------------------------------------
+let sec3_slides = $(".interview_slides li");
+// console.log(sec3_slides);
+let sec3_currentIndex = 0;
+let button3 = $(".interview_content .button_list li");
+let count3 = sec3_slides.length;
+let btn_stop = $(".interview_content .btn_stop");
+let btn_play = $(".interview_content .btn_play");
+
+sec3_slides.eq(sec3_currentIndex).fadeIn();
+let timer3 = setInterval(slidesMove2, 4000);
+
+btn_stop.click(function (e) {
+  e.preventDefault();
+  slideStop3();
+});
+
+btn_play.click(function (e) {
+  e.preventDefault();
+  slidePlay3();
+});
+
+function slidePlay3() {
+  timer3 = setInterval(slidesMove2, 4000);
+  $(btn_stop).addClass("on");
+  $(btn_play).removeClass("on");
+}
+
+// slide stop button
+function slideStop3() {
+  clearInterval(timer3);
+  $(btn_play).addClass("on");
+  $(btn_stop).removeClass("on");
+}
+
+button3.click(function (e) {
+  e.preventDefault();
+  let btnIndex = $(this).index();
+  // console.log(btnIndex);
+  button3.find(".btn").removeClass("on");
+  button3.eq(btnIndex).find(".btn").addClass("on");
+  sec3_slides.stop().fadeOut(1000);
+  sec3_slides.eq(btnIndex).stop().fadeIn(1000);
+  slideStop3();
+
+  if (sec3_currentIndex !== btnIndex) {
+    sec3_currentIndex = btnIndex;
+  }
+});
+
+function slidesMove2() {
+  button3.find(".btn").removeClass("on");
+  let nextIndex = (sec3_currentIndex + 1) % count3;
+  sec3_slides.eq(sec3_currentIndex).fadeOut(1000);
+  sec3_slides.eq(nextIndex).fadeIn(1000);
+  sec3_currentIndex = nextIndex;
+  button3.eq(sec3_currentIndex).find(".btn").addClass("on");
+}
+
+// section3 interview 효과
+for (let i = 0; i < 2; i++) {
+  $(".first").fadeIn(600);
+  $(".fourth").fadeIn(1000);
+  $(".billion_img").fadeIn(1500);
+  $(".first").fadeOut(600);
+  $(".fourth").fadeOut(600);
+  $(".billion_img").fadeOut(600);
+  $(".first").fadeIn(600);
+  $(".fourth").fadeIn(600);
+  $(".billion_img").fadeIn(600);
+
+  $(".first_txt").fadeOut(600);
+  $(".fourth_txt").fadeOut(600);
+  $(".billion_txt").fadeOut(600);
+
+  $(".first_txt").fadeIn(600);
+  $(".fourth_txt").fadeIn(600);
+  $(".billion_txt").fadeIn(600);
+}
+
+// footer----------------------------------------------------------------------------------------------
+$(".btn_quick").on("click", function () {
+  let quickBox = $(".cont_quick");
+  let maxHeight = quickBox.height() + 90;
+  if (quickBox.css("display") != "block") {
+    quickBox.stop(true, false).delay(200).fadeIn(200);
+    $(".footer").stop(true, false).animate({ "padding-top": maxHeight }, 300);
+    /*이렇게 안주면 아래로 떨어져버림.. 
+    scrollTop:원글맨처음부터 현재화면보이는부분까지 길이
+    scrollHeight:전체글의 길이라고 비유해봄.*/
+    $("html, body").animate({ scrollTop: $("html, body").get(0).scrollHeight }, 300);
+  } else {
+    quickBox.stop(true, false).fadeOut(200);
+    $("#footer").stop(true, false).delay(200).animate({ "padding-top": 50 });
+  }
 });
